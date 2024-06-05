1d2b88ed04146c55fac3d5767e5bcd8ffbdbaac7
Alexandre Cormier <88196534+alexandrecormierDT@users.noreply.github.com>
2024-06-05 15:16:39 +0200

    auto scale QRcode

--- src/classes/QRWriter.py
+++ src/classes/QRWriter.py
@@ -11,8 +11,8 @@ class QRWriter ():
         
         ...
     def generate(self,_source_image,_code,_output_folder)->bool:
-        new_image = self.add_qrcode_to_image(_source_image,_code)
-        final_path = self.copy_image_to_output(new_image,_output_folder)
+        new_image = self._add_qrcode_to_image(_source_image,_code)
+        final_path = self._copy_image_to_output(new_image,_output_folder)
         data = self._decode_image(final_path)
         for qr in list(data):
             print(qr)
@@ -23,21 +23,28 @@ class QRWriter ():
     def _decode_image(self,_path):
         data = decode(Image.open(_path))
         return data
+    
+    def _calculate_qr_scale(self,_source_image):
+        source = Image.open(_source_image)
+        scale = source.width*0.0025
+        return scale
+        ...
 
-    def _add_qrcode_to_image(self,_source_image:str,_code:str,_scale:float=2)->str:
-        qrcode = self._generate_image_with_qr_code(str(_code),_scale)
+    def _add_qrcode_to_image(self,_source_image:str,_code:str)->str:
+        qrcode = self._generate_image_with_qr_code(str(_code),self._calculate_qr_scale(_source_image))
         new_image = self._paste_image_in_corners(_source_image,qrcode)
         return new_image
 
-    def _generate_image_with_qr_code(self,_code,_scale:float=2):
+    def _generate_image_with_qr_code(self,_code,_scale:float=8):
         qr = pyqrcode.create(_code)
         path = self._generate_qrcode_image_path(_code)
         qr.png(path, scale=_scale)
+        print(path)
         return path
 
         
     def _paste_image_in_corners(self,_image_under,_image_over):
-        copy = self.copy_image_to_temp(_image_under)
+        copy = self._copy_image_to_temp(_image_under)
         under = Image.open(copy) 
         under_rgb = under.convert('RGB')
         uW = under.width
@@ -82,7 +89,7 @@ class QRWriter ():
     def _generate_qrcode_image_path(self,_code):
         serial = str(uuid.uuid4())[-8:]
         name = f"ImageStamp_{serial}_{_code}"
-        return f"{os.getenv("TEMP")}/{name}.png"
+        return f"{os.getenv('TEMP')}/{name}.png"
 
 
     
